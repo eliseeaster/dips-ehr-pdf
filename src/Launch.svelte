ae2deb045d247040ad7fcf4494b2ef08e5edc022
Elise Easter Christiansen <eec@dips.no>
2021-07-08 15:48:00 +0200

    Endret iss og launch

--- src/Launch.svelte
+++ src/Launch.svelte
@@ -8,10 +8,13 @@
 			scope: 'launch launch/patient patient/DocumentReference.write patient/DocumentReference.read patient/Patient.read patient/encounter patient/resource online_access openid fhirUser',			
 			redirectUri: './app',			
 
-			iss:		  'https://launch.smarthealthit.org/v/r3/sim/'
-			  			+ 'eyJoIjoiMSIsImIiOiJmMDQ2MjkzNi1lYjRiLTRkYT'
-			  			+ 'EtYjQ1YS1mYmQ5NmViZjhjY2IiLCJlIjoic21hcnQt'
-			  			+ 'UHJhY3RpdGlvbmVyLTcxNjE0NTAyIn0/fhir',
+			iss:		  'https://dev-tst.dips.fhir.service.api.dips.no',
+			launch:		  '6c5d9ca9-54d7-42f5-bfae-a7c19cd217f2::DocumentReference/c1c5fdad-eda4-4e9d-89b0-2a7d8eed45cb',
+			<!--launch:		  '6c5d9ca9-54d7-42f5-bfae-a7c19cd217f2::'
+			launch:		  'patientId:encounterId:fhirResource'-->
+			
+
+			<!--https://dev-tst.dips-ehr-pdf.api.dips.no/?iss=&launch=6c5d9ca9-54d7-42f5-bfae-a7c19cd217f2::DocumentReference/c1c5fdad-eda4-4e9d-89b0-2a7d8eed45cb*/-->
 
 		});
 	});
